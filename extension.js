4bb8c6207ebba7ae8caf179f56fb7b06e1db1518
Nathan Smith <nathan.smith@bentley.com>
2018-05-30 16:42:16 -0500

    work

--- extension.js
+++ extension.js
@@ -2,6 +2,18 @@ const vscode = require('vscode');
 const pageBreaks = require('./pageBreaks');
 
 function activate(context) {
+    const settings = vscode.workspace.getConfiguration('homebrewery');
+
+    context.subscriptions.push(vscode.commands.registerCommand('homebrewery.toggle', () =>{
+        settings.update('enabled', !settings.get('enabled'));
+    }));
+
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(cfg => {
+        if(cfg.affectsConfiguration('homebrewery.enabled')) {
+            vscode.commands.executeCommand('markdown.refreshPreview');
+        }
+    }));
+
     return {
         extendMarkdownIt(md) {
             return md.use(pageBreaks);
