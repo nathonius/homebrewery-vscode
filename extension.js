c9bf8db218977c0aa5a15baa4de9e8c90dd32163
Nathan Smith <nathan@praisetopia.org>
2018-05-30 18:07:51 -0500

    it actually works?

--- extension.js
+++ extension.js
@@ -2,15 +2,14 @@ const vscode = require('vscode');
 const pageBreaks = require('./pageBreaks');
 
 function activate(context) {
-    const settings = vscode.workspace.getConfiguration('homebrewery');
-
     context.subscriptions.push(vscode.commands.registerCommand('homebrewery.toggle', () =>{
-        settings.update('enabled', !settings.get('enabled'));
+        const settings = vscode.workspace.getConfiguration('homebrewery');
+        settings.update('enabled', !settings.get('enabled'), true);
     }));
 
     context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(cfg => {
         if(cfg.affectsConfiguration('homebrewery.enabled')) {
-            vscode.commands.executeCommand('markdown.refreshPreview');
+            vscode.commands.executeCommand('markdown.preview.refresh', undefined);
         }
     }));
 
